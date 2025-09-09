52311a66a2ac7f7ed6f59a31812de59913fd1739
samy-belazzoug <samy.belazzoug@laplateforme.io>
2025-09-09 17:18:14 +0200

    Entities created.

---
+++ overlook_hotel/src/main/java/com/OverlookHotel/overlook_hotel/Entity/HoraireEmploye.java
@@ -0,0 +1,24 @@
+package com.OverlookHotel.overlook_hotel.Entity;
+
+import java.util.List;
+
+import org.antlr.v4.runtime.misc.NotNull;
+import org.hibernate.type.descriptor.jdbc.VarcharJdbcType;
+
+import java.sql.Date;
+import java.util.Collections;
+import jakarta.persistence.*;
+
+@Entity
+public class HoraireEmploye {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private Date date;
+    private String shift;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "employee_id")
+    private Employe employee;
+}
---
+++ overlook_hotel/src/main/java/com/OverlookHotel/overlook_hotel/Repository/EmployeeRepository.java
@@ -0,0 +1,5 @@
+package com.OverlookHotel.overlook_hotel.Repository;
+
+public class EmployeeRepository {
+    
+}
