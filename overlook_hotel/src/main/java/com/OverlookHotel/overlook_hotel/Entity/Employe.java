fc63c61fabcc312371c3b7d7fd25728282d6f436
samy-belazzoug <samy.belazzoug@laplateforme.io>
2025-09-15 16:43:23 +0200

    Fixed The method getNom() is undefined for the type Employe kind of error using Lombok.

--- overlook_hotel/src/main/java/com/OverlookHotel/overlook_hotel/Entity/Employe.java
+++ overlook_hotel/src/main/java/com/OverlookHotel/overlook_hotel/Entity/Employe.java
@@ -3,8 +3,12 @@ package com.OverlookHotel.overlook_hotel.Entity;
 import java.util.List;
 import java.util.Collections;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Employe {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
