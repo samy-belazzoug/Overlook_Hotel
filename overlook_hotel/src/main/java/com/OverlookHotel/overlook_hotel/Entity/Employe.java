97b68616c3d47b7c90ccdfeb64a47b49aa0d7934
samy-belazzoug <samy.belazzoug@laplateforme.io>
2025-09-18 12:30:50 +0200

    Implement getters and setters manually to avoid lombok errors

--- overlook_hotel/src/main/java/com/OverlookHotel/overlook_hotel/Entity/Employe.java
+++ overlook_hotel/src/main/java/com/OverlookHotel/overlook_hotel/Entity/Employe.java
@@ -3,12 +3,12 @@ package com.OverlookHotel.overlook_hotel.Entity;
 import java.util.List;
 import java.util.Collections;
 import jakarta.persistence.*;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
+@Data
 @Entity
-@Getter
-@Setter
 public class Employe {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,6 +19,22 @@ public class Employe {
     private String email;
     private String telephone;
     private String poste;
+
+    public String getNom() { return nom; }
+    public void setNom(String nom) { this.nom = nom; }
+
+    public String getPrenom() { return prenom; }
+    public void setPrenom(String prenom) { this.prenom = prenom; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getTelephone() { return telephone; }
+    public void setTelephone(String telephone) { this.telephone = telephone; }
+
+    public String getPoste() { return poste; }
+    public void setPoste(String poste) { this.poste = poste; }
+
 
     @OneToMany(mappedBy = "employe")
     private List<HoraireEmploye> horaires;
--- overlook_hotel/src/main/java/com/OverlookHotel/overlook_hotel/Entity/HoraireEmploye.java
+++ overlook_hotel/src/main/java/com/OverlookHotel/overlook_hotel/Entity/HoraireEmploye.java
@@ -6,22 +6,32 @@ import org.antlr.v4.runtime.misc.NotNull;
 import org.hibernate.type.descriptor.jdbc.VarcharJdbcType;
 
 import java.sql.Date;
+import java.time.LocalDate;
 import java.util.Collections;
 import jakarta.persistence.*;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Getter
-@Setter
 public class HoraireEmploye {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
-    private Date date;
+    private LocalDate date;
     private String shift;
     
+    public LocalDate getDate() { return date; }
+    public void setDate(LocalDate date) { this.date = date; }
+
+    public String getShift() { return shift; }
+    public void setShift(String shift) { this.shift = shift; }
+
+    public Employe getEmploye() { return employe; }
+    public void setEmploye(Employe employe) { this.employe = employe; }
+
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "employe_id")
     private Employe employe;
